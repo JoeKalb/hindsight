9d2141db713b346ddf9de02c179fa6983ae8eaf6
JoeKalb <kalb.joseph@gmail.com>
2018-04-08 08:54:29 -0700

    more changes

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const path = require('path');
+
+
+const port = process.env.PORT || 3000;
+
+app.use('/', express.static(path.join(__dirname + '/')));
+app.get('/', (req, res) => {
+	res.sendFile(__dirname + '/index.html');
+});
+
+http.listen(port, () => {
+    console.log("listening on *:" + port);
+});
