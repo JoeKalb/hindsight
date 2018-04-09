bb34fd89731fb6efc4abba4dc5e4dc446f4b7f28
JoeKalb <kalb.joseph@gmail.com>
2018-04-08 20:35:37 -0700

    final version

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const http = require('http').Server(app);
 const path = require('path');
 
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 
 app.use('/', express.static(path.join(__dirname + '/')));
 app.get('/', (req, res) => {
